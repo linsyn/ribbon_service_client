3a8098c6ebecba6a450e0e5c444f2784aeeb2c2f
linsyn <linym348@gmail.com>
2018-05-04 15:33:29 +0800

    add ribbonServiceClient

---
+++ ribbonServiceClient/src/main/java/client/RibbonController.java
@@ -0,0 +1,26 @@
+package client;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by linsyn on 2018/4/16.
+ */
+@RestController
+public class RibbonController {
+
+        @Autowired
+        private RibbonService ribbonService;
+
+        @GetMapping("/ribbon/{id}")
+        public User findById(@PathVariable Long id) {
+            return this.ribbonService.findById(id);
+        }
+        @GetMapping("/ribbon/list")
+        public String findBook() {
+                return this.ribbonService.findBook();
+        }
+
+}
---
+++ ribbonServiceClient/src/main/java/client/User.java
@@ -0,0 +1,34 @@
+package client;
+
+/**
+ * Created by linsyn on 2018/4/16.
+ */
+public class User {
+    private Long id;
+    private String username;
+    private Integer age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
