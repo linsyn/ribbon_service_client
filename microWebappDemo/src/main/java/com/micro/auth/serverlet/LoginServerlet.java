fe016e1cd8b2c8f318e4824b095fa3c3c5aedb66
linsyn <linym348@gmail.com>
2018-05-07 16:43:37 +0800

    shiro works but not support login and user db auther

---
+++ microWebappDemo/src/main/java/com/micro/auth/serverlet/LoginServerlet.java
@@ -0,0 +1,7 @@
+package com.micro.auth.serverlet;
+
+/**
+ * Created by linsyn on 2018/5/7.
+ */
+public class LoginServerlet {
+}
---
+++ microWebappDemo/src/main/webapp/admin.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>authorized</h2>
+</body>
+</html>
