787edde80095f371acded51f91fb361bd1b25905
linsyn <linym348@gmail.com>
2018-05-09 14:28:43 +0800

    support hibernate

---
+++ microWebappDemo/src/main/java/com/micro/auth/dao/impl/UserDaoImpl.java
@@ -0,0 +1,7 @@
+package com.micro.auth.dao.impl;
+
+/**
+ * Created by linsyn on 2018/5/8.
+ */
+public class UserDaoImpl {
+}
---
+++ microWebappDemo/src/main/java/com/micro/auth/entity/Test.java
@@ -0,0 +1,7 @@
+package com.micro.auth.entity;
+
+/**
+ * Created by linsyn on 2018/5/9.
+ */
+public class Test {
+}
